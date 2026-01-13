367c5ff2ca2f1e16688446ea4172fbb8561d4853
Ajay Rana <ajay07503@gmail.com>
2026-01-14 00:03:34 +0530

    Animated Propose updated

--- SELF/Animated_propose.C
+++ SELF/Animated_propose.C
@@ -10,98 +10,94 @@ void rose(int);
 
 void main()
 {
- int gd=DETECT,gm;
- initgraph(&gd,&gm,"c:\\turboc3\\bgi");
- man();
- getch();
- closegraph();
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "c:\\turboc3\\bgi");
+
+    man();
+
+    getch();
+    closegraph();
 }
 
+/* ---------------- MAN WALKING ---------------- */
 void man()
 {
- int i;
- for(i=1;i<=230;i+=2)
- {
-     cleardevice();
-     women();
+    int i;
 
-     if(i%3==0)
-     {
-         // man
-         circle(100+i,300,15);
-         line(100+i,315,100+i,360);
+    for(i = 1; i <= 230; i += 2)
+    {
+        cleardevice();
+        women();
 
-         // legs
-         line(100+i,360,90+i,400);
-         line(100+i,360,110+i,400);
+        if(i % 3 == 0)
+        {
+            circle(100+i,300,15);
+            line(100+i,315,100+i,360);
 
-         // hands
-         line(100+i,330,90+i,360);
-         line(100+i,330,115+i,340);
-         line(115+i,340,130+i,330);
+            line(100+i,360,90+i,400);
+            line(100+i,360,110+i,400);
 
-         rose(i);
-     }
-     else
-     {
-         // man
-         circle(100+i,300,15);
-         line(100+i,315,100+i,360);
+            line(100+i,330,90+i,360);
+            line(100+i,330,115+i,340);
+            line(115+i,340,130+i,330);
 
-         // legs
-         line(100+i,360,95+i,400);
-         line(100+i,360,105+i,400);
+            rose(i);
+        }
+        else
+        {
+            circle(100+i,300,15);
+            line(100+i,315,100+i,360);
 
-         // hands
-         line(100+i,330,101+i,360);
-         line(100+i,330,115+i,340);
-         line(115+i,340,130+i,330);
+            line(100+i,360,95+i,400);
+            line(100+i,360,105+i,400);
 
-         rose(i);
-     }
+            line(100+i,330,101+i,360);
+            line(100+i,330,115+i,340);
+            line(115+i,340,130+i,330);
 
-     /* ✅ CLOSE CONDITION */
-     if(100+i >= 360)
-     {
-         setcolor(RED);
-         settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-         outtextxy(270,260,"I Love You <3");
-         break;          // stop animation here
-     }
+            rose(i);
+        }
 
-     delay(100);
- }
+        delay(100);
+
+        /* Stop when close */
+        if(100+i >= 360)
+            break;
+    }
+
+    /* ---------------- FINAL TEXT ---------------- */
+    setcolor(RED);
+    settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+    outtextxy(270,260,"I Love You ");
 }
 
+/* ---------------- WOMAN ---------------- */
 void women()
 {
-   line(1,400,800,400);
+    line(1,400,800,400);
 
-   // woman
-   circle(400,300,15);
+    circle(400,300,15);
 
-   // body
-   line(400,315,380,365);
-   line(400,315,420,365);
-   line(380,365,420,365);
+    line(400,315,380,365);
+    line(400,315,420,365);
+    line(380,365,420,365);
 
-   // legs
-   line(390,365,390,400);
-   line(410,365,410,400);
+    line(390,365,390,400);
+    line(410,365,410,400);
 
-   // hand
-   line(400,330,380,350);
+    line(400,330,380,350);
 }
 
+/* ---------------- ROSE ---------------- */
 void rose(int i)
 {
- setcolor(GREEN);
- line(130+i,323,130+i,335);
+    setcolor(GREEN);
+    line(130+i,323,130+i,335);
 
- setcolor(RED);
- circle(130+i,321,2);
- setfillstyle(1,RED);
- floodfill(130+i,321,RED);
+    setcolor(RED);
+    circle(130+i,321,2);
+    setfillstyle(1,RED);
+    floodfill(130+i,321,RED);
 
- setcolor(WHITE);
+    setcolor(WHITE);
 }
